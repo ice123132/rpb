33425549d1b495721061ffdae3125f213f7129c0
ice123132 <ice123132@snu.ac.kr>
2026-04-02 19:22:32 +0900

    Update calc.py

--- calc.py
+++ calc.py
@@ -1,5 +1,18 @@
 def main():
+    print("Let's implement division. Type two numbers for x and y")
+    
+    x = int(input("x > "))
+    y = int(input("y > "))
 
-def add():
+    print("%d / %d = %0.3f" % (x, y, divide(x, y)))
 
-def divide():
+
+def add(a, b):
+    return a + b
+
+
+def divide(x, y):
+    if y == 0:
+        print("Error: cannot divide by zero.")
+        return None
+    return x / y
